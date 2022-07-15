08cb779478cb9bfa8085998f73b0d959a94e1334
Pedro <ph_tavares@hotmail.com>
2022-07-15 09:35:30 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/dev/hroauth/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.dev.hroauth.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class AppConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .csrf()
+                .disable()
+                .exceptionHandling()
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/hr-oauth/**").permitAll();
+
+        // @formatter:on
+    }
+}
---
+++ hr-oauth/src/main/java/com/dev/hroauth/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.dev.hroauth.controllers;
+
+import com.dev.hroauth.entities.User;
+import com.dev.hroauth.services.UserService;
+import feign.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        try {
+            User user = userService.findByEmail(email);
+            return ResponseEntity.ok(user);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
