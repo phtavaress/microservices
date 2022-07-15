df97c1d01443c42104d2f31642b26b4c70222d60
Pedro Henrique Dutra Tavares <ph_tavares@hotmail.com>
2022-07-15 14:12:51 -0300

    Login, jwt token

--- hr-oauth/src/main/java/com/dev/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/dev/hroauth/config/AppConfig.java
@@ -1,25 +1,29 @@
 package com.dev.hroauth.config;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@EnableWebSecurity
-public class AppConfig extends WebSecurityConfigurerAdapter {
 
+@Configuration
+public class AppConfig {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-                .csrf()
-                .disable()
-                .exceptionHandling()
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-                .and()
-                .authorizeRequests()
-                .antMatchers("/hr-oauth/**").permitAll();
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("MY-SECRET-KEY");
+        return tokenConverter;
+    }
 
-        // @formatter:on
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
     }
 }
